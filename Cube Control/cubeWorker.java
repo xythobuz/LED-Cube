3e013b75942adf4311b7a3ba96b78767549da926
Max Nuding <the.short.german@gmail.com>
2011-11-19 10:54:28 +0100

    Renamed the Frame.class to AFrame.class to avoid compatibility issues on Windows. Note: the makefile now won't work! (Yep, I DID change it in there, too!)

--- Cube Control/cubeWorker.java
+++ Cube Control/cubeWorker.java
@@ -45,20 +45,20 @@ public class cubeWorker {
 // Fields
 // --------------------
 
-	private ArrayList<Animation> animations = new ArrayList<Animation>();
-	private int selectedAnimation = 0;
-	private int selectedFrame = 0;
-	private int framesRemaining = 2016; // (128 * 1024) / 65 = 2016,...
-	private boolean changedState = false;
+  private ArrayList<Animation> animations = new ArrayList<Animation>();
+  private int selectedAnimation = 0;
+  private int selectedFrame = 0;
+  private int framesRemaining = 2016; // (128 * 1024) / 65 = 2016,...
+  private boolean changedState = false;
 
 // --------------------
 
   cubeWorker() {
-	animations.add(new Animation());
+  animations.add(new Animation());
   }
 
   cubeWorker(ArrayList<Animation> anims) {
-	  animations = anims;
+    animations = anims;
   }
 
 // --------------------
@@ -68,8 +68,8 @@ public class cubeWorker {
   // Returns how many animations are defined
     public int numOfAnimations() {
            return animations.size();
-    }   
-    
+    }
+
     // Returns how many frames are in the current animation
     public int numOfFrames() {
            return animations.get(selectedAnimation).size();
@@ -88,145 +88,145 @@ public class cubeWorker {
   // Selects an animation on wich the animation specific functions operate
     // Returns -1 if it does not exist, else its index
     public int selectAnimation(int index) {
-		if (animations.size() <= index) {
-			return -1;
-		} else {
-			selectedAnimation = index;
-			return index;
-		}
+    if (animations.size() <= index) {
+      return -1;
+    } else {
+      selectedAnimation = index;
+      return index;
+    }
     }
 
   // Adds a new Animation
     // Returns id if ok, -1 if error or not enough space for
     // another animation
     public int addAnimation() {
-		changedState = true;
-		if (framesRemaining <= 0) {
-        	return -1;
-		} else {
-			int s = animations.size();
-			animations.add(s + 1, new Animation());
-			return s;
-		}
+    changedState = true;
+    if (framesRemaining <= 0) {
+          return -1;
+    } else {
+      int s = animations.size();
+      animations.add(s + 1, new Animation());
+      return s;
     }
-    
+    }
+
     // Removes an animation
     public void removeAnimation() {
-		changedState = true;
-		animations.remove(selectedAnimation);
-		selectedAnimation = 0;
+    changedState = true;
+    animations.remove(selectedAnimation);
+    selectedAnimation = 0;
     }
-    
+
   public String getAnimationName() {
-    	return animations.get(selectedAnimation).getName();
+      return animations.get(selectedAnimation).getName();
     }
 
   public void setAnimationName(String s) {
-	    changedState = true;
-		animations.get(selectedAnimation).setName(s);
+      changedState = true;
+    animations.get(selectedAnimation).setName(s);
     }
 
   public void moveAnimation(int dir) {
-	changedState = true;
+  changedState = true;
     if (dir == UP){
         //animation moved up
-    	if (selectedAnimation > 0) {
-			Animation tmp = animations.get(selectedAnimation);
-			animations.set(selectedAnimation, animations.get(selectedAnimation - 1));
-			animations.set(selectedAnimation - 1, tmp);
-		}
+      if (selectedAnimation > 0) {
+      Animation tmp = animations.get(selectedAnimation);
+      animations.set(selectedAnimation, animations.get(selectedAnimation - 1));
+      animations.set(selectedAnimation - 1, tmp);
+    }
     } else if (dir == DOWN){
       //animation moved down
-		if (selectedAnimation < (animations.size() - 1)) {
-			Animation tmp = animations.get(selectedAnimation);
-			animations.set(selectedAnimation, animations.get(selectedAnimation + 1));
-			animations.set(selectedAnimation + 1, tmp);
-		}
+    if (selectedAnimation < (animations.size() - 1)) {
+      Animation tmp = animations.get(selectedAnimation);
+      animations.set(selectedAnimation, animations.get(selectedAnimation + 1));
+      animations.set(selectedAnimation + 1, tmp);
+    }
     }
   }
 
 // --------------------
 // Frame Specific
 // --------------------
-  
+
   // Selects an animation on wich the frame specific functions operate
     // Returns -1 if it does not exist, else its index
   public int selectFrame(int index) {
-	if (animations.get(selectedAnimation).size() <= index) {
-		return -1;
-	} else {
-		selectedFrame = index;
-		return index;
-	}
+  if (animations.get(selectedAnimation).size() <= index) {
+    return -1;
+  } else {
+    selectedFrame = index;
+    return index;
+  }
   }
 
   public String getFrameName() {
-    	return animations.get(selectedAnimation).get(selectedFrame).getName();
+      return animations.get(selectedAnimation).get(selectedFrame).getName();
     }
 
     public void setFrameName(String s) {
-		changedState = true;
-		animations.get(selectedAnimation).get(selectedFrame).setName(s);
+    changedState = true;
+    animations.get(selectedAnimation).get(selectedFrame).setName(s);
     }
-    
+
     // Adds a Frame to the current animation.
     // Returns id if okay, -1 if error
     public int addFrame() {
-		changedState = true;
-		if (framesRemaining <= 0) {
-			return -1;
-		}
-		framesRemaining--;
-		int s = animations.get(selectedAnimation).size();
-		animations.get(selectedAnimation).add(s);
-		return s;
+    changedState = true;
+    if (framesRemaining <= 0) {
+      return -1;
     }
-    
+    framesRemaining--;
+    int s = animations.get(selectedAnimation).size();
+    animations.get(selectedAnimation).add(s);
+    return s;
+    }
+
     // Remove the frame
     public void removeFrame() {
-		changedState = true;
-		animations.get(selectedAnimation).remove(selectedFrame);
-		selectedFrame = 0;
+    changedState = true;
+    animations.get(selectedAnimation).remove(selectedFrame);
+    selectedFrame = 0;
     }
-    
+
     // Returns array with 64 bytes with led values
     public byte[] getFrame() {
-		return animations.get(selectedAnimation).get(selectedFrame).getData();
-	}
-    
+    return animations.get(selectedAnimation).get(selectedFrame).getData();
+  }
+
     public void setFrame(byte[] data) {
-		changedState = true;
-		animations.get(selectedAnimation).get(selectedFrame).setData(data);
+    changedState = true;
+    animations.get(selectedAnimation).get(selectedFrame).setData(data);
     }
 
-	// Frame duration in 1/24th of a second
-	public byte getFrameTime() {
-		return animations.get(selectedAnimation).get(selectedFrame).getTime();
-	}
+  // Frame duration in 1/24th of a second
+  public byte getFrameTime() {
+    return animations.get(selectedAnimation).get(selectedFrame).getTime();
+  }
 
-	public void setFrameTime(byte time) {
-		changedState = true;
-		animations.get(selectedAnimation).get(selectedFrame).setTime(time);
-	}
+  public void setFrameTime(byte time) {
+    changedState = true;
+    animations.get(selectedAnimation).get(selectedFrame).setTime(time);
+  }
 
   public void moveFrame(int dir){
-	changedState = true;
+  changedState = true;
     if (dir == UP){
         // frame moved up
-      	if (selectedFrame > 0) {
-			Frame tmp = animations.get(selectedAnimation).get(selectedFrame);
-			animations.get(selectedAnimation).set(animations.get(selectedAnimation).get(selectedFrame - 1), selectedFrame);
-			animations.get(selectedAnimation).set(tmp, selectedFrame - 1);
-			selectedFrame--;
-		}
+        if (selectedFrame > 0) {
+      AFrame tmp = animations.get(selectedAnimation).get(selectedFrame);
+      animations.get(selectedAnimation).set(animations.get(selectedAnimation).get(selectedFrame - 1), selectedFrame);
+      animations.get(selectedAnimation).set(tmp, selectedFrame - 1);
+      selectedFrame--;
+    }
     } else if (dir == DOWN){
       // frame moved down
-		if (selectedFrame < (animations.get(selectedAnimation).size() - 1)) {
-			Frame tmp = animations.get(selectedAnimation).get(selectedFrame);
-			animations.get(selectedAnimation).set(animations.get(selectedAnimation).get(selectedFrame + 1), selectedFrame);
-			animations.get(selectedAnimation).set(tmp, selectedFrame + 1);
-			selectedFrame++;
-		}
+    if (selectedFrame < (animations.get(selectedAnimation).size() - 1)) {
+      AFrame tmp = animations.get(selectedAnimation).get(selectedFrame);
+      animations.get(selectedAnimation).set(animations.get(selectedAnimation).get(selectedFrame + 1), selectedFrame);
+      animations.get(selectedAnimation).set(tmp, selectedFrame + 1);
+      selectedFrame++;
+    }
     }
   }
 
@@ -236,19 +236,19 @@ public class cubeWorker {
 
     // Loads an animation file into this object
     public int loadState(String path) {
-			changedState = false;
+      changedState = false;
 
-    		return 0;
+        return 0;
     }
-    
+
     // Saves the state of this object in an animation file
     public int saveState(String path) {
            changedState = false;
-		   AnimationUtility.writeFile(path, animations);
-		   if (AnimationUtility.getLastError() != null) {
-			   System.out.println(AnimationUtility.getLastError());
-			   return -1;
-			}
+       AnimationUtility.writeFile(path, animations);
+       if (AnimationUtility.getLastError() != null) {
+         System.out.println(AnimationUtility.getLastError());
+         return -1;
+      }
            return 0;
     }
 
@@ -260,19 +260,19 @@ public class cubeWorker {
 // --------------------
 // Serial Port Specific
 // --------------------
-    
+
     // sends state of object to led cube
     public int uploadState(String port) {
 
            return 0;
     }
-    
+
     // loads all state from the cube into this object
     public int downloadState(String port) {
 
            return 0;
     }
-        
+
     public String[] getSerialPorts() {
 
            String[] sPorts = { "Select serial port..." }; // Has to be the first entry
@@ -284,159 +284,159 @@ public class cubeWorker {
 }
 
 class AnimationUtility {
-	private static String lastError = null;
-	
-	public static ArrayList<Animation> readFile(String path) {
-		return null;
-	}
+  private static String lastError = null;
 
-	public static void writeFile(String path, ArrayList<Animation> animations) {
-		File f = new File(path);
-		if (f.exists()) {
-			try {
-				f.delete();
-			} catch (Exception e) {
-				lastError = e.toString();
-				return;
-			}
-		}
-		FileWriter output = null;
-		try {
-			output = new FileWriter(f);
-			for (int i = 0; i < animations.size(); i++) {
-				writeAnimation(animations.get(i), output);
-			}
-		} catch (Exception e) {
-			lastError = e.toString();
-			return;
-		} finally {
-			if (output != null) {
-				try {
-					output.close();
-				} catch (Exception e) {
-					lastError = e.toString();
-				}
-			}
-		}
+  public static ArrayList<Animation> readFile(String path) {
+    return null;
+  }
 
-	}
+  public static void writeFile(String path, ArrayList<Animation> animations) {
+    File f = new File(path);
+    if (f.exists()) {
+      try {
+        f.delete();
+      } catch (Exception e) {
+        lastError = e.toString();
+        return;
+      }
+    }
+    FileWriter output = null;
+    try {
+      output = new FileWriter(f);
+      for (int i = 0; i < animations.size(); i++) {
+        writeAnimation(animations.get(i), output);
+      }
+    } catch (Exception e) {
+      lastError = e.toString();
+      return;
+    } finally {
+      if (output != null) {
+        try {
+          output.close();
+        } catch (Exception e) {
+          lastError = e.toString();
+        }
+      }
+    }
 
-	public static String getLastError() {
-		String tmp = lastError;
-		lastError = null;
-		return tmp;
-	}
+  }
 
-	private static void writeAnimation(Animation anim, FileWriter f) throws IOException {
-		f.write(anim.getName() + "\n");
-		for (int i = 0; i < anim.size(); i++) {
-			writeFrame(anim.get(i), f);
-		}
-		f.write("\n");
-	}
+  public static String getLastError() {
+    String tmp = lastError;
+    lastError = null;
+    return tmp;
+  }
 
-	private static void writeFrame(Frame fr, FileWriter f) throws IOException {
-		for (int i = 0; i < 8; i++) {
-			writeLayer(fr.getLayer(i), f);
-		}
-		f.write(Integer.toString( (fr.getTime() & 0xff) + 0x100, 16).substring(1) + "\n");
-	}
+  private static void writeAnimation(Animation anim, FileWriter f) throws IOException {
+    f.write(anim.getName() + "\n");
+    for (int i = 0; i < anim.size(); i++) {
+      writeFrame(anim.get(i), f);
+    }
+    f.write("\n");
+  }
 
-	private static void writeLayer(byte[] l, FileWriter f) throws IOException {
-		String hex = "";
-		for (int i = 0; i < l.length; i++) {
-			hex += Integer.toString( (l[i] & 0xff) + 0x100, 16).substring(1);
-		}
-		hex += "\n";
+  private static void writeFrame(AFrame fr, FileWriter f) throws IOException {
+    for (int i = 0; i < 8; i++) {
+      writeLayer(fr.getLayer(i), f);
+    }
+    f.write(Integer.toString( (fr.getTime() & 0xff) + 0x100, 16).substring(1) + "\n");
+  }
 
-		f.write(hex);
-	}
+  private static void writeLayer(byte[] l, FileWriter f) throws IOException {
+    String hex = "";
+    for (int i = 0; i < l.length; i++) {
+      hex += Integer.toString( (l[i] & 0xff) + 0x100, 16).substring(1);
+    }
+    hex += "\n";
+
+    f.write(hex);
+  }
 }
 
-class Frame {
-	private byte[] data = new byte[64];
-	private byte duration = 1;
-	private String name = "Frame";
-	
-	String getName() {
-		return name;
-	}
+class AFrame {
+  private byte[] data = new byte[64];
+  private byte duration = 1;
+  private String name = "Frame";
 
-	void setName(String s) {
-		name = s;
-	}
+  String getName() {
+    return name;
+  }
 
-	void setData(byte[] d) {
-		data = d;
-	}
+  void setName(String s) {
+    name = s;
+  }
 
-	byte[] getData() {
-		return data;
-	}
+  void setData(byte[] d) {
+    data = d;
+  }
 
-	void setTime(byte t) {
-		duration = t;
-	}
+  byte[] getData() {
+    return data;
+  }
 
-	byte getTime() {
-		return duration;
-	}
+  void setTime(byte t) {
+    duration = t;
+  }
 
-	byte[] getLayer(int i) {
-		return Arrays.copyOfRange(data, (i * 8), (i * 8) + 8);
-	}
+  byte getTime() {
+    return duration;
+  }
+
+  byte[] getLayer(int i) {
+    return Arrays.copyOfRange(data, (i * 8), (i * 8) + 8);
+  }
 }
 
 class Animation {
-	private ArrayList<Frame> frames = new ArrayList<Frame>();
-	private int lastFrameIndex = 0;
-	private String name = "Animation";
+  private ArrayList<AFrame> frames = new ArrayList<AFrame>();
+  private int lastFrameIndex = 0;
+  private String name = "Animation";
 
-	String getName() {
-		return name;
-	}
+  String getName() {
+    return name;
+  }
 
-	void setName(String s) {
-		name = s;
-	}
+  void setName(String s) {
+    name = s;
+  }
 
-	Frame get(int i) {
-		try {
-			return frames.get(i);
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println(e.toString());
-			return null;
-		}
-	}
+  AFrame get(int i) {
+    try {
+      return frames.get(i);
+    } catch (IndexOutOfBoundsException e) {
+      System.out.println(e.toString());
+      return null;
+    }
+  }
 
-	void set(Frame f, int i) {
-		if (lastFrameIndex <= i) {
-			try {
-				frames.set(i, f);
-			} catch (IndexOutOfBoundsException e) {
-				System.out.println(e.toString());
-			}
-		}
-	}
+  void set(AFrame f, int i) {
+    if (lastFrameIndex <= i) {
+      try {
+        frames.set(i, f);
+      } catch (IndexOutOfBoundsException e) {
+        System.out.println(e.toString());
+      }
+    }
+  }
 
-	void remove(int i) {
-		try {
-			frames.remove(i);
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println(e.toString());
-		}
-	}
+  void remove(int i) {
+    try {
+      frames.remove(i);
+    } catch (IndexOutOfBoundsException e) {
+      System.out.println(e.toString());
+    }
+  }
 
-	void add(int i) {
-		try {
-			frames.add(i, new Frame());
-			lastFrameIndex++;
-		} catch (IndexOutOfBoundsException e)  {
-			System.out.println(e.toString());
-		}
-	}
+  void add(int i) {
+    try {
+      frames.add(i, new AFrame());
+      lastFrameIndex++;
+    } catch (IndexOutOfBoundsException e)  {
+      System.out.println(e.toString());
+    }
+  }
 
-	int size() {
-		return frames.size();
-	}
+  int size() {
+    return frames.size();
+  }
 }
